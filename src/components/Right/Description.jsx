a9941999ee2d84a5742b3ce61771b869d3bba921
Mateus <mtsschiavi@gmail.com>
2023-10-03 14:54:41 -0300

    updated

--- src/components/Right/Description.jsx
+++ src/components/Right/Description.jsx
@@ -7,16 +7,78 @@ function Description() {
                 Schiavi, Mateus de Sousa
                 <p>Empleo</p>
                 <p className='busca'>Estoy buscando una oportunidad de empleo en el campo de la programación full-stack.
-                Tengo un nivel avanzado de inglés y español, y actualmente estoy aprendiendo japonés, italiano, alemán y coreano.</p>
+                    Tengo un nivel avanzado de inglés y español, y actualmente estoy aprendiendo japonés, italiano, alemán y coreano.</p>
             </div>
             <div className='profession'>
                 <p>EXPERIENCIA PROFISIONAL</p>
                 <hr />
                 <div className='data'>
                     <p>Julio/2014 - Octubre/2014</p>
+                    <p className='resumo'>
+                        Ayudante de Maestro de matemática
+                        y portugués en Kumon de Suzano
+                    </p>
                     <p>Agosto/2021 - Octubre/2021</p>
-                    <p>Enero/2022 - Febrero/2022</p>
+                    <p className='resumo'>
+                        Ayudante de producción en
+                        la empresa Itaquá Resistencias
+                    </p>
                     <p>Febrero/2023 - Actualmente</p>
+                    <p className='resumo'>
+                        Maestro de Inglés en
+                        Aliança América Idiomas
+                    </p>
+                </div>
+            </div>
+            <div className='education'>
+                <p>EDUCACIÓN</p>
+                <hr />
+                <div className='curso'>
+                    <p>
+                        electricista de mantenimiento electronico - SENAI SUZANO
+                    </p>
+                    <p>
+                        Fecha de Inicio: Julio/2019
+                        Fecha de cierre: Julio/2021
+                    </p>
+                    <p>
+                        Inglés - CCAA Suzano
+                    </p>
+                    <p>
+                        Fecha de Inicio: Octube/2006
+                        Fecha de cierre: Deciembre/2016
+                    </p>
+                    <p>
+                        Español - CCAA Suzano
+                    </p>
+                    <p>
+                        Fecha de Inicio: Julio/2012
+                        Fecha de cierre: Deciembre/2015
+                    </p>
+                    <p>
+                        Desarrollo Web - FUNVAL Internacional</p>
+                    <p>
+                        Julio/2023 - Actualmente
+                    </p>
+                    <p>
+                        Diseño grafico - Smart People Suzano
+                    </p>
+                    <p>
+                        Fecha de Inicio: Julio/2014
+                        Fecha de cierre: Julio/2015
+                    </p>
+                    <p>
+                        Ingeniería en Computación - UNIVESP Suzano
+                    </p>
+                    <p>
+                        Julio/2022 - Actualmente
+                    </p>
+                    <p>
+                        Applied Technology - BYU Idaho
+                    </p>
+                    <p>
+                        Marzo/2022 - Actualmente
+                    </p>
                 </div>
             </div>
         </div>
